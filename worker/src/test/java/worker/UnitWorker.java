99ed4037c080b37c2e686b39fec652fd294b438a
THAMARAIKANNAN S <thamaraikannansrinivasan@gmail.com>
2025-12-12 12:41:37 +0530

    added source with docker, compose

---
+++ worker/src/test/java/worker/UnitWorker.java
@@ -0,0 +1,26 @@
+package worker;
+import org.junit.jupiter.api.Test;
+ 
+class UnitWorker {
+ 
+    @Test
+    void sample1() {
+ 
+    }
+
+
+    @Test
+    void sample2() {
+
+    }
+
+    @Test
+    void sample3() {
+
+    }
+
+    @Test
+    void sample4() {
+
+    }
+}
